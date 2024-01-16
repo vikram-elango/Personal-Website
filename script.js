2856ca1af233e9180e4b9a3445033da8908ff934
Vikram <velango@ucsb.edu>
2024-01-16 10:30:16 -0800

    revamped website- new design/theme/animations

--- script.js
+++ script.js
@@ -9,26 +9,32 @@ const totalSkills = document.querySelectorAll('.carousel-track .skill').length;
 carouselTrack.style.width = `calc(100% * (${totalSkills} / 5))`;
 
 document.addEventListener("DOMContentLoaded", function () {
-    const navLinks = document.querySelectorAll(".nav-item");
-  
-    navLinks.forEach((link) => {
-      link.addEventListener("click", smoothScroll);
-    });
-  
-    function smoothScroll(e) {
+  const navLinks = document.querySelectorAll(".nav-item");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
       e.preventDefault();
-  
-      const targetId = this.getAttribute("href").substring(1); 
+
+      const headerOffset = document.querySelector('.navbar').offsetHeight; // Height of header plus some extra space
+      const targetId = e.target.getAttribute("href").substring(1);
       const targetElement = document.getElementById(targetId);
-  
+
       if (targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop,
-          behavior: "smooth", 
+        const elementRect = targetElement.getBoundingClientRect();
+        const absoluteElementTop = elementRect.top + window.pageYOffset;
+        const middle = absoluteElementTop - headerOffset;
+
+        window.scrollBy({
+          top: middle - window.pageYOffset + 100,
+          behavior: 'smooth'
         });
       }
-    }
+    });
   });
+});
+
+
+
 
 
 
@@ -41,4 +47,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   
   setInterval(shiftItem, interval);
+  
+
   
